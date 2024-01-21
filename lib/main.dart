2cd4cb451d758e625e6ef0bc5eaa81e65fe007ee
Sabin Poudel <sabinpoudel6969@gmail.com>
2024-01-21 21:56:29 +0530

    connected app with firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,25 @@
 import 'package:budgetbuddy/constants/app_theme.dart';
 import 'package:budgetbuddy/constants/rotutes.dart';
+import 'package:budgetbuddy/firebase_options.dart';
 import 'package:budgetbuddy/view_models/forgot_password_view_model.dart';
 import 'package:budgetbuddy/view_models/login_view_model.dart';
 import 'package:budgetbuddy/view_models/signup_view_model.dart';
 import 'package:budgetbuddy/views/boarding/boarding_view.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:provider/provider.dart';
 
 void main() async {
-  runApp(const MyApp());
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   SystemChrome.setPreferredOrientations([
     DeviceOrientation.portraitUp,
   ]);
+  runApp(const MyApp());
 }
 
 late Size size;
