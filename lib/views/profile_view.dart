e94429596fea3508abccf8f0b5a3cf1b88f2aa51
Sabin Poudel <sabinpoudel6969@gmail.com>
2024-01-29 22:10:44 +0530

    removed card widget from profile page

--- lib/views/profile_view.dart
+++ lib/views/profile_view.dart
@@ -40,160 +40,143 @@ class _ProfileViewState extends State<ProfileView> {
               SizedBox(
                 height: 10.h,
               ),
-              Card(
-                elevation: 2,
-                child: Container(
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(
-                      10.w,
+              Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(
+                    10.w,
+                  ),
+                ),
+                width: double.infinity,
+                child: Column(
+                  children: [
+                    CircleAvatar(
+                      radius: 60.w,
+                      backgroundImage: widget.me!.profileImageUrl == null
+                          ? const AssetImage(
+                              AppImagePath.defaultAvatar,
+                            )
+                          : NetworkImage(
+                              widget.me!.profileImageUrl!,
+                            ) as ImageProvider,
                     ),
-                  ),
-                  width: double.infinity,
-                  child: Column(
-                    children: [
-                      CircleAvatar(
-                        radius: 60.w,
-                        backgroundImage: widget.me!.profileImageUrl == null
-                            ? const AssetImage(
-                                AppImagePath.defaultAvatar,
-                              )
-                            : NetworkImage(
-                                widget.me!.profileImageUrl!,
-                              ) as ImageProvider,
-                      ),
-                      SizedBox(
-                        height: 20.h,
-                      ),
-                      Text(
-                        FirebaseServies.firebaseAuth.currentUser!.email!,
-                        style: Theme.of(context).textTheme.labelSmall!.copyWith(
-                              fontSize: 16.w,
-                              fontWeight: FontWeight.bold,
-                              color: Colors.black,
-                            ),
-                        softWrap: true,
-                      ),
-                      SizedBox(
-                        height: 10.h,
-                      ),
-                    ],
-                  ),
+                    SizedBox(
+                      height: 20.h,
+                    ),
+                    Text(
+                      FirebaseServies.firebaseAuth.currentUser!.email!,
+                      style: Theme.of(context).textTheme.labelSmall!.copyWith(
+                            fontSize: 16.w,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.black,
+                          ),
+                      softWrap: true,
+                    ),
+                    SizedBox(
+                      height: 10.h,
+                    ),
+                  ],
                 ),
               ),
               SizedBox(
                 height: 10.h,
               ),
-              Card(
-                elevation: 2,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(
-                    10.w,
-                  ),
-                ),
-                shadowColor: Colors.black,
-                child: Padding(
-                  padding:
-                      EdgeInsets.symmetric(horizontal: 20.w, vertical: 30.h),
-                  child: Column(
-                    children: [
-                      TextFormField(
-                        style: Theme.of(context).textTheme.labelSmall!.copyWith(
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 20.w, vertical: 30.h),
+                child: Column(
+                  children: [
+                    TextFormField(
+                      style: Theme.of(context).textTheme.labelSmall!.copyWith(
+                            fontSize: 14.w,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.black,
+                          ),
+                      initialValue: widget.me!.displayName,
+                      decoration: InputDecoration(
+                        prefixIcon: Icon(
+                          CupertinoIcons.person_alt_circle,
+                          size: 26.w,
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 10.h,
+                    ),
+                    TextFormField(
+                      style: Theme.of(context).textTheme.labelSmall!.copyWith(
+                            fontSize: 14.w,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.black,
+                          ),
+                      maxLines: null,
+                      initialValue: widget.me!.description,
+                      decoration: InputDecoration(
+                        prefixIcon: Icon(
+                          CupertinoIcons.info,
+                          size: 26.w,
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 10.h,
+                    ),
+                    ListTile(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(5.w),
+                      ),
+                      leading: Icon(
+                        Icons.upgrade,
+                        size: 26.w,
+                      ),
+                      onTap: () {},
+                      title: Text(
+                        "Export Data",
+                        style: Theme.of(context).textTheme.bodySmall!.copyWith(
                               fontSize: 14.w,
                               fontWeight: FontWeight.bold,
                               color: Colors.black,
                             ),
-                        initialValue: widget.me!.displayName,
-                        decoration: InputDecoration(
-                          prefixIcon: Icon(
-                            CupertinoIcons.person_alt_circle,
-                            size: 26.w,
-                          ),
-                        ),
                       ),
-                      SizedBox(
-                        height: 10.h,
+                      subtitle: Text(
+                        "Get data into your local machine",
+                        style: Theme.of(context).textTheme.bodySmall!.copyWith(
+                              fontSize: 12.w,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.black,
+                            ),
                       ),
-                      TextFormField(
-                        style: Theme.of(context).textTheme.labelSmall!.copyWith(
+                    ),
+                    Divider(
+                      thickness: 2.w,
+                    ),
+                    ListTile(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(5.w),
+                      ),
+                      leading: Icon(
+                        Icons.logout_outlined,
+                        size: 26.w,
+                      ),
+                      onTap: () {
+                        profileViewModel.signOut(context);
+                      },
+                      title: Text(
+                        "Logout",
+                        style: Theme.of(context).textTheme.bodySmall!.copyWith(
                               fontSize: 14.w,
                               fontWeight: FontWeight.bold,
                               color: Colors.black,
                             ),
-                        maxLines: null,
-                        initialValue: widget.me!.description,
-                        decoration: InputDecoration(
-                          prefixIcon: Icon(
-                            CupertinoIcons.info,
-                            size: 26.w,
-                          ),
-                        ),
                       ),
-                      SizedBox(
-                        height: 10.h,
+                      subtitle: Text(
+                        "Logout from this device",
+                        style: Theme.of(context).textTheme.bodySmall!.copyWith(
+                              fontSize: 12.w,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.black,
+                            ),
                       ),
-                      ListTile(
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(5.w),
-                        ),
-                        leading: Icon(
-                          Icons.upgrade,
-                          size: 26.w,
-                        ),
-                        onTap: () {},
-                        title: Text(
-                          "Export Data",
-                          style:
-                              Theme.of(context).textTheme.bodySmall!.copyWith(
-                                    fontSize: 14.w,
-                                    fontWeight: FontWeight.bold,
-                                    color: Colors.black,
-                                  ),
-                        ),
-                        subtitle: Text(
-                          "Get data into your local machine",
-                          style:
-                              Theme.of(context).textTheme.bodySmall!.copyWith(
-                                    fontSize: 12.w,
-                                    fontWeight: FontWeight.bold,
-                                    color: Colors.black,
-                                  ),
-                        ),
-                      ),
-                      Divider(
-                        thickness: 2.w,
-                      ),
-                      ListTile(
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(5.w),
-                        ),
-                        leading: Icon(
-                          Icons.logout_outlined,
-                          size: 26.w,
-                        ),
-                        onTap: () {
-                          profileViewModel.signOut(context);
-                        },
-                        title: Text(
-                          "Logout",
-                          style:
-                              Theme.of(context).textTheme.bodySmall!.copyWith(
-                                    fontSize: 14.w,
-                                    fontWeight: FontWeight.bold,
-                                    color: Colors.black,
-                                  ),
-                        ),
-                        subtitle: Text(
-                          "Logout from this device",
-                          style:
-                              Theme.of(context).textTheme.bodySmall!.copyWith(
-                                    fontSize: 12.w,
-                                    fontWeight: FontWeight.bold,
-                                    color: Colors.black,
-                                  ),
-                        ),
-                      ),
-                    ],
-                  ),
+                    ),
+                  ],
                 ),
               ),
             ],
